2fae2df1e3d4372e20b03e3ecea830e1a23d9cb8
redmasmas <ogultekin16@posta.pau.edu.tr>
2021-02-28 08:11:16 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("İsim: " + customer.Name);
+            Console.WriteLine("Soyisim: " + customer.Surname);
+            Console.WriteLine("Telefon: " + customer.Telefon);
+            Console.WriteLine("Müşteri eklendi.");
+            Console.WriteLine("----------------");
+            
+        }
+        public void Delete(int id,Customer customer)
+        {
+            Console.WriteLine(id+"Numarali musteri silindi.");
+        }
+
+        public void List(Customer customer)
+        {
+            Console.WriteLine("İsim: " + customer.Name);
+            Console.WriteLine("Soyisim: " + customer.Surname);
+            Console.WriteLine("Telefon: " + customer.Telefon);
+            Console.WriteLine("----------------");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+           
+
+            Customer[] customers = new Customer[] { customer1};
+     
+            CustomerManager customerManager = new CustomerManager();
+            int number = 0;
+            Console.WriteLine("Yapacaginiz islemi seciniz:");
+            Console.WriteLine("Musteri silmek icin 1 'e basiniz");
+            Console.WriteLine("Musteri listelemek icin 2 'ye basiniz");
+            Console.WriteLine("Musteri eklemek icin 3 'e basiniz");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("Musteri id giriniz");
+                    int Id=Convert.ToInt32(Console.ReadLine());
+                    customerManager.Delete(Id, customer1);
+                    break;
+                case 2:
+                    customerManager.List(customer1);
+                    break;
+                case 3:
+                    Console.WriteLine("Musteri id giriniz.");
+                    customer1.Id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Musteri adi giriniz.");
+                    customer1.Name = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Musteri soyadi giriniz.");
+                    customer1.Surname = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Musteri telefon giriniz.");
+                    customer1.Telefon = Convert.ToString(Console.ReadLine());
+                    customerManager.Add(customer1);
+                    break;
+            }
+
+        }
+    }
+}
